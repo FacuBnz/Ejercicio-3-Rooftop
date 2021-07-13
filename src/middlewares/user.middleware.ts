aa3047288b958b50bc13c5f19a546c7554e54b7f
FacuBnz <facu.j.benitez@gmail.com>
2021-07-13 18:37:38 -0300

    Se agrego un else en la condicion

--- src/middlewares/user.middleware.ts
+++ src/middlewares/user.middleware.ts
@@ -5,7 +5,9 @@ export const autorizationUser = (req: Request, res: Response, next: NextFunction
     if(!req.headers.authorization || req.headers.authorization !== "Bearer 65a83e72c7e990a3e6565ae8b7cc071c"){
         
         res.status(401).send("Unauthorized");
+    }else{
+
+        next();
     }
 
-    next();
 }
