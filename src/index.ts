4bbb791503221898b61c139305458cc18079e293
FacuBnz <facu.j.benitez@gmail.com>
2021-07-14 03:00:36 -0300

    Se agrego el uso de variables de entorno y se agregaron imports

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
+require('dotenv').config()
 import express from 'express'
 import { autorizationUser } from './middlewares/user.middleware'
+import { log } from './middlewares/log.middleware'
 import userRoutes  from './routes/user.routes'
 
 const app = express()
 
 //middlewares
 app.use(express.json())
+app.use(log)
 app.use(autorizationUser, userRoutes)
 
-app.listen(3000)
-console.log('Server on port', 3000);
+app.listen(process.env.PORT)
+console.log('Server on port', process.env.PORT);
